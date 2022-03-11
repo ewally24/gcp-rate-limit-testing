55b0ec3a0ce9b8a69ba30b62de6cd60d3b991946
ewally24 <ewally24@gmail.com>
2022-03-11 13:42:14 -0500

    Added the coinDesk rate limit test function

--- controller/swarmController.js
+++ controller/swarmController.js
@@ -1,5 +1,39 @@
+const axios = require('axios');
+
+const sleep = async function() {
+  console.log('sleep for 2 second...');
+
+  return new Promise(function(resolve, reject){
+    setTimeout(function(){
+      resolve(true);
+    }, 1000)
+  })
+}
+
 const swarmServer = async function(req, res) {
-  res.send('Swarming the server, send our best troops!');
+  let coinDataArray = []
+  let reachLimit = true;
+
+  while(!reachLimit) {
+    await axios(`https://api.coindesk.com/v1/bpi/currentprice.json`, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'}
+    }).then(async function(response){
+      if (response.data && coinDataArray.length <= 500) {
+        // console.log('response: ', response.data);
+        console.log('coinDataArray length: ', coinDataArray.length);
+        coinDataArray.push(response.data.bpi.USD);
+      } else {
+        reachLimit = true;
+        console.log('reached coin desk data limit!');
+        return res.send(coinDataArray);
+      }
+    }).catch(err => {
+      console.log("There's been retrieving coin desk data: ", err)
+      return res.send(false);
+    });
+  }
+
 }
 
 module.exports = {
