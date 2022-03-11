5fa2bf1195cc161bb4c37b07f038c77f0a1be2ab
ewally24 <ewally24@gmail.com>
2022-03-11 14:15:22 -0500

    Fix function for reachLimit while condition

--- controller/swarmController.js
+++ controller/swarmController.js
@@ -12,26 +12,31 @@ const sleep = async function() {
 
 const swarmServer = async function(req, res) {
   let coinDataArray = []
-  let reachLimit = true;
+  let reachLimit = false;
 
   while(!reachLimit) {
-    await axios(`https://api.coindesk.com/v1/bpi/currentprice.json`, {
-      method: 'GET',
-      headers: {'Content-Type': 'application/json'}
-    }).then(async function(response){
-      if (response.data && coinDataArray.length <= 500) {
-        // console.log('response: ', response.data);
-        console.log('coinDataArray length: ', coinDataArray.length);
-        coinDataArray.push(response.data.bpi.USD);
-      } else {
-        reachLimit = true;
-        console.log('reached coin desk data limit!');
-        return res.send(coinDataArray);
-      }
-    }).catch(err => {
-      console.log("There's been retrieving coin desk data: ", err)
-      return res.send(false);
-    });
+    try {
+      await axios(`https://api.coindesk.com/v1/bpi/currentprice.json`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
+      }).then(async function(response){
+        console.log('response: ', response);
+        if (response.data && coinDataArray.length <= 500) {
+          // console.log('response: ', response.data);
+          console.log('coinDataArray length: ', coinDataArray.length);
+          coinDataArray.push(response.data.bpi.USD);
+        } else {
+          reachLimit = true;
+          console.log('reached coin desk data limit!');
+          return res.send(coinDataArray);
+        }
+      }).catch(err => {
+        console.log("There's been retrieving coin desk data: ", err)
+        return res.send(false);
+      });
+    } catch(err) {
+      res.json({error: `Error while making coindesk endpoint call ${err}`})
+    }
   }
 
 }
